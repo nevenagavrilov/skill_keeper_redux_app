e3fab1a0f2e842f06693b14a0133ab90a1f64886
Nevena <gavrilovnevena@yahoo.com>
2022-07-28 10:20:20 +0200

    "remove from cart" button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,28 @@
+import { Button } from "@mui/material";
 import { useSelector } from "react-redux";
 import classes from "./Cart.module.css";
+import { cartActions } from "./cartSlice";
+import { useDispatch } from "react-redux";
 
 const Cart = () => {
   const profilesInCart = useSelector((state) => state.cart.profilesInCart);
+  
+  const dispatch = useDispatch();
 
   return (
     <div className={classes.cart}>
       {profilesInCart.map((profile) => (
-        <li key={profile.profileId}>{profile.profileName}</li>
+        <li key={profile.profileId}>
+          {profile.profileName}
+          <Button variant="outlined"
+            onClick={() => {
+              dispatch(
+                cartActions.removeProfileFromCart(profile.id));
+            }}
+          >
+            remove from cart
+          </Button>
+        </li>
       ))}
     </div>
   );
